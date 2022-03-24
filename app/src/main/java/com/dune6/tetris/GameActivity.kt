84df98b30dd46b60f2caf511ffe93aced1198582
dune6 <ilya.voznesensky@gmail.com>
2022-03-24 20:34:03 +0300

    Realise UI for main menu

--- app/src/main/java/com/dune6/tetris/GameActivity.kt
+++ app/src/main/java/com/dune6/tetris/GameActivity.kt
@@ -2,10 +2,35 @@ package com.dune6.tetris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.dune6.tetris.storage.AppPreferences
 
 class GameActivity : AppCompatActivity() {
+
+    var tvHighScore: TextView? = null
+    var tvCurrentScore: TextView? = null
+    var appPreferences: AppPreferences? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        appPreferences = AppPreferences(this)
+
+        val btnRestart = findViewById<Button>(R.id.btn_restart)
+        tvHighScore = findViewById<TextView>(R.id.tv_high_score)
+        tvCurrentScore = findViewById<TextView>(R.id.tv_current_score)
+
+        updateHighScore()
+        updateCurrentScore()
     }
+
+    private fun updateCurrentScore() {
+        tvCurrentScore?.text = "0"
+    }
+
+    private fun updateHighScore() {
+        tvHighScore?.text = "${appPreferences?.getHighScore()}"
+    }
+
 }
